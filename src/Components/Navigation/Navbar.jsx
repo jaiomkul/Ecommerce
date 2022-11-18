b495cb8e9521e9c659da22752a9f09c8f4733108
jaiomkul <94021305+jaiomkul@users.noreply.github.com>
2022-11-19 02:36:55 +0530

    navbar

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -1,8 +1,57 @@
 export const Navbar = () => {
   return (
-    <div>
-      Helolo
-      <h1>dffffffffffffff</h1>
+    <div className="bg-[#24a3b5] shadow-lg">
+      <div className="container mx-auto">
+        <div className="sm:flex justify-center">
+          {/* Logo */}
+          <a href="" className="text-[#AECFC1] text-3xl font-bold p-3">
+            LOGO
+          </a>
+
+          {/* Menu Items */}
+
+          <ul className="flex-1 text-white sm:self-center text-xl font-medium border-t sm:border-none">
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                Home
+              </a>
+            </li>
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                Man
+              </a>
+            </li>
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                Women
+              </a>
+            </li>
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                New Arrivals
+              </a>
+            </li>
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                About
+              </a>
+            </li>
+          </ul>
+
+          <ul className="flex flex-row-reverse flex-1 text-white sm:self-center text-xl font-medium border-t sm:border-none">
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                SIGN UP
+              </a>
+            </li>
+            <li className="sm:inline-block">
+              <a href="" className="p-5 hover:bg-white hover:text-blue-600/100">
+                LOG IN
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
